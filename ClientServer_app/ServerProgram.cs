7d2c244497914625d6177f131a44a2caa943075b
Natalija Markvarte <natalija.markvarte@bbit.tech>
2020-11-13 14:32:48 +0200

    mapped memory

--- ClientServer_app/ServerProgram.cs
+++ ClientServer_app/ServerProgram.cs
@@ -2,7 +2,8 @@
 using System.IO;
 using System.IO.Pipes;
 using System.Threading;
-
+using System.IO.MemoryMappedFiles;
+public enum ServerProgramType { FIFO, queue, mappedMemory }
 
 namespace ClientServer_ServerApp
 {
@@ -44,16 +45,53 @@ namespace ClientServer_ServerApp
 
         static void Main(string[] args)
         {
+            ServerProgramType c = ServerProgramType.mappedMemory; //  for now
+            switch (c) {
+            case ServerProgramType.FIFO: // 0
+                    //Klients sūta ziņojumus, bet serveris saņem, tiek veidoti jauni pavedieni, kuri atgriež klienta informāciju
 
-            //Klients sūta ziņojumus, bet serveris saņem, tiek veidoti jauni pavedieni, kuri atgriež klienta informāciju
-
-            ServerProgram Server = new ServerProgram();
+                    ServerProgram Server = new ServerProgram();
 
 
-            Thread ServerThread = new Thread(Server.ThreadStartServer);
+                    Thread ServerThread = new Thread(Server.ThreadStartServer);
 
 
-            ServerThread.Start();
+                    ServerThread.Start();
+            break;
+
+            case ServerProgramType.queue: // 1
+                    
+
+            break;
+
+            case ServerProgramType.mappedMemory: // 2
+                    //Tiek palaists serveris
+
+                    Console.WriteLine("Memory mapped failu serveris");
+
+                    //Tiek izveidots dalītās atmiņas mehānisms un ierakstīta informācija failā
+
+                    using (var file = MemoryMappedFile.CreateNew("myFile", int.MaxValue))
+                    {
+                        var bytes = new byte[24];
+                        for (var i = 0; i < bytes.Length; i++)
+                            bytes[i] = (byte)(65 + i);
+
+                        using (var writer = file.CreateViewAccessor(0, bytes.Length))
+                        {
+                            writer.WriteArray<byte>(0, bytes, 0, bytes.Length);
+                        }
+                        Console.WriteLine("Palaidiet memory mapped failu lasītāju pirms izejiet no programmas");
+
+                        Console.ReadLine();
+                    }
+            break;
+
+                default:
+                    Console.WriteLine("Wrong server type.");
+                    break;
+            }
+         
 
         }
 
